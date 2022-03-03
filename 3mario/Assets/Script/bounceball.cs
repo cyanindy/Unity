58a61bb40d798093a59b8e3a79926166b69e9f0e
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2022-03-04 02:48:30 +0900

    LostArk_3rdMario_week5_day23

---
+++ 3mario/Assets/Script/bounceball.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounceball : MonoBehaviour
+{
+    
+    public float bounceball_speed=1.0f; // 이동속도
+    Vector3 pos; //현재위치
+    float delta = 5.0f; // 좌(우)로 이동가능한 (x)최대값
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = pos;
+        float acceleration = delta * Mathf.Abs(Mathf.Sin(Time.time * bounceball_speed));
+        // 좌우 이동의 최대치 및 반전 처리를 이렇게 한줄에 멋있게 하네요.
+        v.y += acceleration;
+
+        transform.position = v;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player controller = other.GetComponent<player>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
---
+++ 3mario/Assets/Script/player.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class player : MonoBehaviour 
+{
+    Rigidbody2D rigidbody2d;
+    public float player_Speed = 60f;
+    public int player_max_health=3;
+    int player_current_health;
+    void Start() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        player_current_health=player_max_health;
+    }
+
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 position = rigidbody2d.position;
+
+        position.x = position.x + player_Speed * horizontal * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        player_current_health = Mathf.Clamp(player_current_health + amount, 0, player_max_health);
+        Debug.Log(player_current_health + "/" + player_max_health);
+    }
+
+    public void falling() {
+        player_current_health=0;
+        Debug.Log(player_current_health + "/" + player_max_health);
+    }
+
+}//class end
+
+
+
+
+/*
+public class player : MonoBehaviour
+{
+
+    public float maxSpeed = 100f;
+    public float Speed = 50f;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    private bool moveLeft = false;
+    private bool moveRight = false;
+    private bool isMoving = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            moveRight = false;
+            moveLeft = false;
+            isMoving = false;
+            animator.SetTrigger("Attack");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            moveLeft = true;
+            moveRight = false;
+
+            if (!isMoving)
+            {
+                isMoving = true;
+                animator.SetFloat("Direction", -1);
+                animator.SetTrigger("Move");
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveRight = true;
+            moveLeft = false;
+
+            if (!isMoving)
+            {
+                isMoving = true;
+                animator.SetFloat("Direction", 1);
+                animator.SetTrigger("Move");
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            isMoving = false;
+            moveRight = false;
+            moveLeft = false;
+            animator.SetTrigger("Stand");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float move = 0;
+
+        if (moveLeft)
+            move = -1;
+        else if (moveRight)
+            move = 1;
+        
+
+        if (move != 0)
+        {
+            if (Mathf.Abs(rigidBody.velocity.x) < maxSpeed)
+            {
+                Vector2 toMove = new Vector2(move * Speed, rigidBody.velocity.y);
+                rigidBody.AddForce(toMove);
+            }
+            
+        }
+        else if(move == 0)
+        {
+            rigidBody.velocity = new Vector2(0, 0);
+        }
+    }
+}
+*/
